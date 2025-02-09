36c127d484744ae566bddd42f9afb8068f92add1
sabarni.d <sabarni.d@media.net>
2025-02-09 13:32:30 +0530

    initial commit

---
+++ components/MDXComponents.tsx
@@ -0,0 +1,18 @@
+import TOCInline from 'pliny/ui/TOCInline'
+import Pre from 'pliny/ui/Pre'
+import BlogNewsletterForm from 'pliny/ui/BlogNewsletterForm'
+import type { MDXComponents } from 'mdx/types'
+import { Sandpack } from "@codesandbox/sandpack-react";
+import Image from './Image'
+import CustomLink from './Link'
+import TableWrapper from './TableWrapper'
+
+export const components: MDXComponents = {
+  Image,
+  TOCInline,
+  a: CustomLink,
+  pre: Pre,
+  table: TableWrapper,
+  BlogNewsletterForm,
+  Sandpack,
+}
